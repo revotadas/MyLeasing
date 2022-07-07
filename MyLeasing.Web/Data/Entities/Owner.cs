e6bd0a5658e1f2400f290f31f65ce35f5090f97f
Revotadas <rafael.lopes24@gmail.com>
2022-07-07 14:24:54 +0100

    Criação Owners Controller

---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Web.Data.Entities {
+    public class Owner {
+        public int Id { get; set; }
+
+        [Required]
+        public string Document { get; set; }
+
+        [Required]
+        [Display(Name = "Owner Name")]
+        [MaxLength(50)]
+        public string OwnerName { get; set; }
+
+        [Display(Name = "Fixed Phone")]
+        [MinLength(9, ErrorMessage = "Fixed Phone must have 9 digits.")]
+        [MaxLength(9)]
+        public string FixedPhone { get; set; }
+
+        [Required]
+        [Display(Name = "Cell Phone")]
+        [MinLength(9, ErrorMessage = "Cell Phone must have 9 digits.")]
+        [MaxLength(9)]
+        public string CellPhone { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
---
+++ MyLeasing.Web/Migrations/20220707130242_InitDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyLeasing.Web.Migrations
+{
+    public partial class InitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Document = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OwnerName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FixedPhone = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: true),
+                    CellPhone = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Owners");
+        }
+    }
+}
