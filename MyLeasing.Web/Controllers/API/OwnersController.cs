cce50ca6d8965b8dff0b99cfeacffe7c71594bab
Revotadas <rafael.lopes24@gmail.com>
2022-07-21 13:33:35 +0100

    API OwnersController

---
+++ MyLeasing.Web/Controllers/API/OwnersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyLeasing.Web.Data;
+
+namespace MyLeasing.Web.Controllers.API {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnersController : Controller {
+        private readonly IOwnersRepository ownersRepository;
+
+        public OwnersController(IOwnersRepository ownersRepository) {
+            this.ownersRepository = ownersRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetOwners() {
+            return Ok(ownersRepository.GetAll());
+        }
+    }
+}
